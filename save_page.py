d37dd97eb7d1cc42613ef6f2166e0c49e377ff83
grachev21 <grachev613@gmail.com>
2023-12-07 16:55:55 +0400

    parser

--- save_page.py
+++ save_page.py
@@ -16,32 +16,29 @@ def save(link):
     print(link, '<<<link')
 
     def saveName():
-        value = bs.find('h1', {'itemprop': 'name'}).get_text()
+        value = str(bs.find('h1', {'itemprop': 'name'}).get_text())
+        value = value.replace('\n', '')
+        value = value.replace(' ', '')
         return value
 
     def savePrice():
-        value = bs.find('div', {'id': 'results-price'}).get_text()
+        value = str(bs.find('div', {'id': 'results-price'}).get_text())
         return value
      
     def saveDescription():
-        value = bs.find('div', {'class': 'mobile-tabler-content'})
+        value = str(bs.find('div', {'class': 'mobile-tabler-content'}))
         return value
-    
-    def saveCharacteristic():
-        value = bs.find('div', {'class': 'tabler-slick slick-initialized slick-slider'})
-        print(value, '<<<')
-        exit()
-        return value
-  
 
-    def saveImage():
+    def saveImage(name):
+        # Сохранение фотографий
+        os.mkdir(f'db_data/{name}/')
         listImage = []
         count = 1
         for img in bs.find_all('img', {'src': re.compile('.*400_400.*.jpg')}):
             if 'src' in img.attrs:
-                print(f"https://mirm.ru{img.attrs['src']}")
                 image_save = requests.get(f"https://mirm.ru{img.attrs['src']}")
-                out = open(f'{count}.jpg', "wb")
+                print(name)
+                out = open(f'db_data/{name}/{count}.jpg', 'wb')
                 out.write(image_save.content)
                 out.close()
                 count += 1
@@ -51,26 +48,16 @@ def save(link):
     name = saveName()
     price = savePrice()
     description = saveDescription()
-    image = saveImage()
-    
-    # # Сохранение фотографий
-    os.mkdir(f'db_data/{name}/')
-     
-    num_name = 0
-    for i in image:
-        image_save = requests.get(i)
-        out = open(f'db_data/{name}-{key}/name_{num_name}.jpg', "wb")
-        out.write(image_save.content)
-        out.close()
-        sleep(2)
-        num_name += 1
+    print(name)
+    saveImage(name)
     
     json_data = {
-            'description': description,
-            'characteristic': characteristic,
             'name': name,
-            'price': price
+            'price': price,
+            'description': description
             }
     
     with open('db_json.json', 'a') as f:
-        json.dump(json_data, f, sort_keys=true, indent=2)
+        json.dump(json_data, f, sort_keys=True, indent=2)
+
+    exit()
